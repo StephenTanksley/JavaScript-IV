34a093221db293193c2227c1c84c6ef7212471d8
Stephen Tanksley <stephen.tanksley@gmail.com>
2019-07-11 18:56:29 -0500

    Finished the prototype refactor. Getting started on the Lambda Classes portion of the assignment. Working with Jenny and Steven.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,13 @@
 // CODE here for your Lambda Classes
+
+class Person {
+    constructor(attributes) {
+      this.name = attributes.name;
+      this.age = attributes.age;
+      this.location = attributes.location;
+    }
+  
+    destroy() {
+      return `${this.name} was removed from the game.`;
+    };
+  };
