11904f90623b15369d0505f2c5d3e106abd1680d
Stephen Tanksley <stephen.tanksley@gmail.com>
2019-07-11 20:47:00 -0500

    Nearly finished with the homework.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -34,9 +34,9 @@ class Person {
       return `Today we are learning about ${subject}`;
     };
 
-    // grade(student, subject) {
-    //     `${student.name} receives a perfect score on ${subject}.`;
-    // };
+    grade(student, subject) {
+        return `${student.name} receives a perfect score on ${subject}.`;
+    };
   };
 
   const fred = new Instructor({
@@ -50,7 +50,48 @@ class Person {
 
   console.log(fred);
   console.log(fred.demo("JavaScript"));
-  console.log(fred.grade(student, subject))
-  //need to create a student object and test the grade method.
+//   console.log(fred.grade(stephen, 'JavaScript')) 
+//Must be logged after the creation of the student object.
 
-  
+
+  class Student extends Person {
+    constructor(personAttributes) {
+        super(personAttributes);
+      this.previousBackground = personAttributes.previousBackground;
+      this.className = personAttributes.className;
+      this.favSubjects = personAttributes.favSubjects;
+    };
+
+    listsSubjects() {
+        return this.favSubjects.join(', ');
+      };
+
+      PRAssignment(subject) {
+            return `${this.name} has submitted a PR for ${subject}`;
+          };
+
+      sprintChallenge(subject) {
+        return `${this.name} has begun sprint challenge on ${subject}`
+          };
+    };
+
+const stephen = new Student( {
+    name: 'Stephen',
+    age: 31,
+    location: 'Chicago',
+    favSubjects: ['Javascript', 'HTML', 'CSS', 'LESS'],
+    className: 'WEBPT8',
+    previousBackground: 'Musician'
+});
+
+//Need to test Instructor's grade() method.
+console.log(fred.grade(stephen, 'JavaScript'))
+
+
+console.log(stephen);
+
+console.log(stephen.listsSubjects());
+
+console.log(stephen.PRAssignment('JavaScript'));
+
+console.log(stephen.sprintChallenge('JavaScript'));
